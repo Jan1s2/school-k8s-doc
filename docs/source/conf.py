ce2c93af09249fb434adbc4c0df89d36a02a2e52
Jan Str <janstr06@gmail.com>
2024-09-21 21:38:57 +0200

    fix copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Školní k8s dokumentace'
+copyright = '2024, it2125'
+author = 'it2125'
 
 release = '0.1'
 version = '0.1.0'
